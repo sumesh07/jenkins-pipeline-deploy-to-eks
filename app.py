ab04e7c7df6f7388e4740656420f1646f6d006a0
sumesh07 <91238421+sumesh07@users.noreply.github.com>
2024-02-20 00:33:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to nginx! Thank you for using nginx.'
+    return 'Welcome to nginx!
+If you see this page, the nginx web server is successfully installed and working. Further configuration is required.
+
+For online documentation and support please refer to nginx.org.
+Commercial support is available at nginx.com.
+
+Thank you for using nginx.'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
