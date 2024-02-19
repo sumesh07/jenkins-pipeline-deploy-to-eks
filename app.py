06f87c6dab1c40a97bbf5b3b4152eaf0e0dc74b2
sumesh07 <91238421+sumesh07@users.noreply.github.com>
2024-02-20 00:27:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Welcome to nginx!
-
-Thank you for using nginx.'
+    return 'Welcome to nginx! Thank you for using nginx.'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
